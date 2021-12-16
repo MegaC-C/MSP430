ead30bbe259212230f3bc62f17e72be5bef0d4dd
MegaC-C <59622754+MegaC-C@users.noreply.github.com>
2021-12-16 14:39:45 +0100

    Update MSP430i2030_UART_TX_interruptSwitch.cpp

--- MSP430i2030_UART_TX_interruptSwitch.cpp
+++ MSP430i2030_UART_TX_interruptSwitch.cpp
@@ -1,72 +1,4 @@
-/* --COPYRIGHT--,BSD_EX
- * Copyright (c) 2013, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *******************************************************************************
- * 
- *                       MSP430 CODE EXAMPLE DISCLAIMER
- *
- * MSP430 code examples are self-contained low-level programs that typically
- * demonstrate a single peripheral function or device feature in a highly
- * concise manner. For this the code may rely on the device's power-on default
- * register values and settings such as the clock configuration and care must
- * be taken when combining code from several examples to avoid potential side
- * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
- * for an API functional library-approach to peripheral configuration.
- *
- * --/COPYRIGHT--*/
-//******************************************************************************
-// MSP430i20xx Initialization Routines - low_level_init.c
-//
-// This function is called by the start-up code before "main" is called, and
-// before data segment initialization is performed. The function affects the
-// following modules:
-//  - JTAG - JTAG is disabled
-//  - TLV - A TLV checksum is performed
-//  - PMM - The shared reference is calibrated to 1.16V
-//  - Clock System - The DCO is calibrated to 16.384MHz
-//  - SD24 - The SD24 reference voltage trimming is calibrated
-//
-// The return value of this function controls if data segment initialization
-// should take place. If 0 is returned, it is bypassed.
-//
-// Any code example or application written for the MSP430i20xx which takes
-// advantage of any of the affected modules is suggested to include this
-// function to ensure full calibration of the application.
-//
-// Additionally, this initialization routine is suggested for small, short 
-// code examples to ensure the JTAG will be unlocked. The i20xx series of 
-// devices requires execution of 64 MCLK cycles before the JTAG can be unlocked.
-// If a device does not execute 64 MCLK cycles, it cannot be accessed via JTAG.
-//******************************************************************************
-#include "msp430.h"
+#include <msp430.h> 
 
 #ifdef __TI_COMPILER_VERSION__
 int _system_pre_init(void)
@@ -149,38 +81,44 @@ int system_pre_init(void)
 }
 
 
-
-int main (void){
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	// -- LED output --
-	P2DIR |= BIT4;
-	
-	// -- switch input --
-	P2DIR &= ~BIT3;
-	P2IES |= BIT3;
-	P2IE |= BIT3;
-	P2IFG &= ~BIT3;
-	
-	// -- uart --
-	UC0CTLW0 |= UCSWRST;
-	UC0CTLW0 |= UCSSEL__SMCLK;
-	UCA0BRW = 8;
-	
-	P1SEL1 &= ~BIT3;
-	P1SEL0 |= BIT3;
-	
-	UCA0MCTLW |= F7E1;
-		
-	__enable_interrupt();
-	
-	while(1){};
-	
-	#pragma vector = ;
-	__interrupt void toggleTX(void){
-		P2OUT ^= BIT4;
-		UCA0TXBUF = 0b10101010;
-		P2IFG &= ~BIT3;
-	}
-	
+	    P2DIR |= BIT4;
+
+	    // -- switch input --
+	    P2DIR &= ~BIT3;
+	    P2IES |= BIT3;
+	    P2IFG &= ~BIT3;
+	    P2IE |= BIT3;
+
+
+	    // -- uart --
+	    UCA0CTLW0 |= UCSWRST;
+	    UCA0CTLW0 |= UCSSEL__SMCLK;
+	    UCA0BRW = 8;
+	    UCA0MCTLW |= 0xF7E1;
+
+	    P1SEL1 &= ~BIT3;
+	    P1SEL0 |= BIT3;
+
+	    UCA0CTLW0 &= ~UCSWRST;
+
+	    __enable_interrupt();
+
+	    while(1){};
+
+
+
 	return 0;
 }
+
+#pragma vector = PORT2_VECTOR;
+__interrupt void toggleTX(void){
+    P2OUT ^= BIT4;
+    UCA0TXBUF = 'A';
+    P2IFG &= ~BIT3;
+    }
+
