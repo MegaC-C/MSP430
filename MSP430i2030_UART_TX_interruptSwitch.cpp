c5763f92bdcba01a892212639eba267053c7f92a
root <root@localhost>
2021-12-16 13:28:59 +0100

    done

--- MSP430i2030_UART_TX_interruptSwitch.cpp
+++ MSP430i2030_UART_TX_interruptSwitch.cpp
@@ -2,7 +2,31 @@
 
 int main (){
 	
+	// -- LED output --
+	P2DIR |= BIT4;
 	
+	// -- switch input --
+	P2DIR &= ~BIT3;
+	P2IES |= BIT3;
+	P2IE |= BIT3;
+	P2IFG &= ~BIT3;
+	
+	// -- uart --
+	UC0CTLW0 |= UCSWRST;
+	UC0CTLW0 |= UCSSEL__SMCLK;
+	UCA0BRW = 8;
+	UCA0MCTLW |= F7E1;
+		
+	_enable_interrupts();
+	
+	while(1){};
+	
+	#pragma vector = ;
+	__interrupt void toggleTX(void){
+		P2OUT ^= BIT4;
+		UCA0TXBUF = 0b10101010;
+		P2IFG &= ~BIT3;
+	}
 	
 	return 0;
 }
