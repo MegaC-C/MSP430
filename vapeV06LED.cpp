0c609b3936e770edf1f868ebad4044bf3a302f54
root <root@localhost>
2022-03-31 08:39:02 +0200

    FirstCommit

---
+++ vapeV06LED.cpp
@@ -0,0 +1,28 @@
+void LED(char color){
+	P2OUT &= ~BIT0 & ~BIT5 & ~BIT1;
+	switch(color){
+		case 'r':
+			P2OUT |= BIT0;
+			break;
+		case 'y':
+			P2OUT |= BIT0  | BIT5;
+			break;
+		case 'g':
+			P2OUT |= BIT5;
+			break;
+		case 't':
+			P2OUT |= BIT5 | BIT1;
+			break;
+		case 'b':
+			P2OUT |= BIT1;
+			break;
+		case 'p':
+			P2OUT |= BIT0  | BIT1;
+			break;
+		case 'o':
+			break;
+		default:
+			P2OUT |= BIT0  | BIT5 | BIT1;
+			break;
+	}
+}
